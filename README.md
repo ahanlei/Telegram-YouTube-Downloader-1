3bc1f8a431f39f9d88539e5f16c87858c92a77ad
mastermindvrtx <mastermindvrtx@gmail.com>
2021-05-09 09:00:07 -0400

    Beta

---
+++ README.md
@@ -0,0 +1,30 @@
+#                                   . YoutubeDownloadBot .
+* Commands YouTube for Audio & Video and sends it to telegram after receiving valid URL [Do not forwarded any just copy and paste the link]
+* Reach me on Telegram [@YoutubeDownloadVrtx_Bot](https://github.com/phantomXhawk/YouTubeDownloader.git)
+<p align="centre">
+  <img src="https://telegra.ph/file/8e47e1e5f501555bc0e59.jpg">
+</p>
+
+# |-------------------------------------------------->💋Use: 
+* PERSONAL ---- Copy any Valid Youtube link and paste inside the bot and follow the prompts.
+* GROUPS ------ Add the bot then copy any valid Youtube link and paste inside the bot and follow the prompts.
+
+**THE BIGGER THE SIZE,THE MORE TIME YOU HAVE TO WAIT**
+**File size more then 2gb won't be downloaded due to telegram policy**
+
+## Prerequisite: 
+* ffmpeg - [ https://github.com/phantomXhawk/heroku-buildpack-ffmpeg-latest.git ]
+  
+    
+## Dependencies:
+* pip3 install -r requirements.txt
+
+
+## Initialise
+* Change configuration config.py in heroku vars
+* Install dependencies
+* python3 -m YoutubeDL
+   
+# |-------------------------------------------------->🔥Heroku:
+* The easiest way to deploy [@YoutubeDownloadVrtx_Bot](https://github.com/phantomXhawk/YouTube-Downloader.git)
+* [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/phantomXhawk/YouTubeDownloader.git/tree/dev)
---
+++ workers/youtube.py
@@ -0,0 +1,92 @@
+"""
+|---------------------------------------------------_____________$$$
+|---------------------------------------------------_____________$$$$
+|---------------------------------------------------_____________$$$$
+|---------------------------------------------------_____________$$$$$
+|---------------------------------------------------_____________$$$$$$
+|---------------------------------------------------_____________$$$$$$$
+|---------------------------------------------------_____________$$$$$$$$
+|---------------------------------------------------_____________$$$$$$$$$
+|---------------------------------------------------____________$$$__$$$$$$
+|---------------------------------------------------____________$$$___$$$$$$
+|---------------------------------------------------____________$$$____$$$$$
+|---------------------------------------------------____________$$$_____$$$$$
+|---------------------------------------------------____________$$$______$$$$
+|---------------------------------------------------____________$$$_______$$$$
+|---------------------------------------------------____________$$$_______$$$$
+|---------------------------------------------------____________$$$________$$$
+|---------------------------------------------------____________$$$________$$$
+|---------------------------------------------------____________$$$________$$$
+|---------------------------------------------------____________$$$________$$
+|---------------------------------------------------____________$$________$$$
+|---------------------------------------------------____________$$_______$$$
+|---------------------------------------------------____________$$______$$$
+|---------------------------------------------------_____$$$$$$$$$_____$$$
+|---------------------------------------------------___$$$$$$$$$$$___$$$
+|---------------------------------------------------_$$$$$$$$$$$$$__$$$
+|---------------------------------------------------$$$$$$$$$$$$$$$$$
+|---------------------------------------------------$$$$$$$$$$$$$
+|---------------------------------------------------$$$$$$$$$$$$
+|---------------------------------------------------_$$$$$$$$$
+|---------------------------------------------------___$$$$
+
+ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ
+"""
+
+from datetime import datetime, timedelta
+from pyrogram import Client, Filters, InlineKeyboardMarkup, InlineKeyboardButton
+from Tg_YtDl import user_time
+from config import youtube_next_fetch
+from func.ytdlfunc import extractYt, create_buttons
+import wget
+import os
+from PIL import Image
+
+ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
+
+@Client.on_message(Filters.regex(ytregex))
+async def ytdl(_, message):
+    userLastDownloadTime = user_time.get(message.chat.id)
+    try:
+        if userLastDownloadTime > datetime.now():
+            wait_time = round((userLastDownloadTime - datetime.now()).total_seconds() / 60, 2)
+            await message.reply_text(f"`Wait {wait_time} Minutes before next Request`")
+            return
+    except:
+        pass
+
+    url = message.text.strip()
+    await message.reply_chat_action("typing")
+    try:
+        title, thumbnail_url, formats = extractYt(url)
+
+        now = datetime.now()
+        user_time[message.chat.id] = now + \
+                                     timedelta(minutes=youtube_next_fetch)
+
+    except Exception:
+        await message.reply_text("`Failed To Fetch Youtube Data...😔\nWait for 2-5mins")
+        return
+    buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
+    sentm = await message.reply_text("Analyzing the url 🧠👀🧠👀")
+    try:
+        # Todo add webp image support in thumbnail by default not supported by pyrogram
+        # https://www.youtube.com/watch?v=lTTajzrSkCw
+        img = wget.download(thumbnail_url)
+        im = Image.open(img).convert("RGB")
+        output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
+        if not os.path.isdir(output_directory):
+            os.makedirs(output_directory)
+        thumb_image_path = f"{output_directory}.jpg"
+        im.save(thumb_image_path,"jpeg")
+        await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
+        await sentm.delete()
+    except Exception as e:
+        print(e)
+        try:
+            thumbnail_url = "https://telegra.ph/file/8e47e1e5f501555bc0e59.jpg"
+            await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
+        except Exception as e:
+            await sentm.edit(
+            f"<code>{e}</code> #Error")
