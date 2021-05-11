7f38f518766c7bb8efa7c19ee975a7b1ece276c9
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-11 14:39:23 +0530

    Update ping.py

--- workers/ping.py
+++ workers/ping.py
@@ -1,24 +1,73 @@
-import os
-import telebot
-import config
+from time import time
+from datetime import datetime
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message
 
-bot = telebot.TeleBot(TOKEN)
-# tb.send_message(chatid, message)
-bot.send_message(chat.id, 'GoGo power ranger')
-
-ip = ["10.5.110.55", "10.5.110.56"]
-#ip = ['google.com', 'facebook.com']
-
-for val in ip:
-    response = os.system('ping -c 1 ' + val)
-    if response == 0:
-        print(val + ' is up!')
-        bot.send_message(chat.id, val + ' is up')
-    else:
-        print(val + ' is down!')
-        bot.send_message(chat.id, val + ' is down!')
-        
-bot.sendChatAction(
-    chat_id = chat.id,
-    action = "typing"
+# DELAY_DELETE = 60
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
 )
+
+self_or_contact_filter = filters.create(
+    lambda
+    _,
+    __,
+    message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
+
+# https://gist.github.com/borgstrom/936ca741e885a1438c374824efb038b3
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(filters.text
+                   & self_or_contact_filter
+                   & ~filters.edited
+                   & ~filters.via_bot
+                   & filters.regex("^.alive$"))
+async def ping_pong(_, m: Message):
+    """reply ping with pong and delete both messages"""
+    start = time()
+    m_reply = await m.reply_text("...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        f"""**一═デ︻ 𝕊𝕚𝕟𝕘𝕖𝕣𝕍𝕣𝕥𝕩 ︻デ═一**\n[🍺](https://telegra.ph/file/1d858bae5f9c4c178bcfb.jpg)
+by~ @mastermindvrtx        
+**I am alive and ready to play in vc✌🏻**:
+        `{delta_ping * 1000:.3f}ms`"""
+    )
+    
+@Client.on_message(filters.text
+                   & self_or_contact_filter
+                   & ~filters.edited
+                   & ~filters.via_bot
+                   & filters.regex("^.cpu$"))
+async def get_uptime(_, m: Message):
+    """/uptime Reply with readable uptime and ISO 8601 start time"""
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        f"""**一═デ︻ 𝕊𝕚𝕟𝕘𝕖𝕣𝕍𝕣𝕥𝕩 ︻デ═一**\n
+[🍺](https://telegra.ph/file/1d858bae5f9c4c178bcfb.jpg)[🍺]
+**Uptime**:
+           `{uptime}`\n
+**Start time**:
+              `{START_TIME_ISO}`"""   
+    )
