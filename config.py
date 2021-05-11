a540b24702720af133845a47c0e9c3ee1b52291d
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-11 14:14:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import  os
 TOKEN = os.environ.get("TOKEN")
 APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
+CHAT_ID = os.environ.get("CHAT_ID")
+
 
 youtube_next_fetch = 0  # time in minute
 
