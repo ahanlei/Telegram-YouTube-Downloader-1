903bf6cf846345e4088f54d6d137c22480729250
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-11 14:27:46 +0530

    Update ping.py

--- workers/ping.py
+++ workers/ping.py
@@ -1,17 +1,24 @@
 import os
 import telebot
+from config import TOKEN, CHAT_ID
 
-channel = '-XXXXXXXXX'
-token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-bot = telebot.TeleBot(token)
-ip = ["10.5.110.55", "10.5.109.151", "10.5.110.56"]
-#ip = ['google.com', 'yandex.ru', 'facebook.com']
+bot = telebot.TeleBot(TOKEN)
+# tb.send_message(chatid, message)
+bot.send_message(CHAT_ID, 'GoGo power ranger')
+
+ip = ["10.5.110.55", "10.5.110.56"]
+#ip = ['google.com', 'facebook.com']
 
 for val in ip:
     response = os.system('ping -c 1 ' + val)
     if response == 0:
         print(val + ' is up!')
-        bot.send_message(channel, val + ' is up')
+        bot.send_message(CHAT_ID, val + ' is up')
     else:
         print(val + ' is down!')
-        bot.send_message(channel, val + ' is down!')
+        bot.send_message(CHAT_ID, val + ' is down!')
+        
+bot.sendChatAction(
+    chat_id = CHAT_ID,
+    action = "typing"
+)
