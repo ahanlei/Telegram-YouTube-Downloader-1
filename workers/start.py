7167baa57cce9cab8c564681929a9c5b0b2f201a
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-12 18:33:41 +0000

    Update start.py

--- workers/start.py
+++ workers/start.py
@@ -15,10 +15,10 @@
 ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ
 """
 
-from pyrogram import Client, filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Client.on_message(filters.command(["start"]), group=-2)
+@Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
