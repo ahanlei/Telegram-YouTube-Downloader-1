12acba0e8bff1843507c0daa881b6d970e25673b
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-13 20:59:17 +0530

    Update start.py

--- workers/start.py
+++ workers/start.py
@@ -33,5 +33,21 @@ async def start(client, message):
     Use the below button or type /help for More info.
     [📥](https://telegra.ph/file/39812237fd7a1bfc02532.jpg)"""
 
+    
+@Client.on_message(Filters.command(["start@vrtxytbot"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("⛓**𝔾𝕣𝕠𝕦𝕡**⛓:", url="https://t.me/musicvrtx")],
+        [InlineKeyboardButton("⚙️**⚙𝕌𝕡𝕕𝕒𝕥𝕖_ℂ𝕙𝕒𝕟𝕟𝕖𝕝**⚙️:", url="https://t.me/vrtxwork")],
+        [InlineKeyboardButton("📨**𝔾𝕚𝕥ℍ𝕦𝕓**📨", url="https://t.me/vrtxwork")],
+        [InlineKeyboardButton("🧬**𝕆𝕨𝕟𝕖𝕣**🧬", url="https://t.me/mastermindvrtx")],
+    ])
+    welcomed = f"""
+    🎈Dear,
+        Sir,Ma'am  <b>{message.from_user.first_name}</b>
+    Use the below button or type /help for More info.
+    [📥](https://telegra.ph/file/39812237fd7a1bfc02532.jpg)"""    
+    
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
